48c2355fb270bdde27d7b13fae0544e56ee5e851
Oliver Proud <23120916+oliverproud@users.noreply.github.com>
2019-08-06 14:39:02 +0100

    Cleaned up comments

--- script.py
+++ script.py
@@ -12,15 +12,19 @@ def index():
 
     # Displays the shown string above the user entered text
     header_review = "Review:"
+
     # Displays the show string above the model determined sentiment
     header_sentiment = "Sentiment:"
 
     print(request.args)
+
     # Contains a dictionary containing the parsed contents of the query string
     if(request.args):
+
         # Passes contents of query string to the prediction function contained in model.py
         x_input, prediction = predict_sentiment(request.args['text_in'])
         print(prediction[0]['prob'])
+
         # Indexes the returned dictionary for the sentiment probability
         if((prediction[0]['prob']) > 0.5):
             prediction = "Positive"
@@ -28,6 +32,7 @@ def index():
         else:
             prediction = "Negative"
             return flask.render_template('index.html', text_in=x_input, prediction=prediction, header_review=header_review, header_sentiment=header_sentiment)
+
     # If the parsed query string does not contain anything then return index page
     else:
         return flask.render_template('index.html')
